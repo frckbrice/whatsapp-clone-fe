c54d0d0f3b23072e8d33adbf430c799c9786b2ce
gmarvis <sgmarvis@gmail.com>
2023-11-07 16:59:17 +0100

    ive created the sigup page and have rendered it on the index page i think that is where all the pages will be conditionaly rendered base on user auth

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,10 +10,14 @@ module.exports = {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
         themecolor: "#01a884",
+        secondry: "#005c4b",
         btn: {
           background: "hsl(var(--btn-background))",
           "background-hover": "hsl(var(--btn-background-hover))",
         },
+      },
+      screens: {
+        mobile: "0px",
       },
     },
   },
