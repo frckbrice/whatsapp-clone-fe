204821d769b52ff9c32f3f200bc375fcd59b3ca2
frckbrice <maebrie2017@gmail.com>
2023-11-09 13:26:20 +0100

    adding menu popops

--- components/discossions.tsx
+++ components/discossions.tsx
@@ -9,21 +9,42 @@ import { IoSendSharp } from "react-icons/io5";
 import { IoMdAdd } from "react-icons/io";
 import DropDown from "./mainLayoutPage/DropDown";
 import HeaderMainPageL from "./mainLayoutPage/HeaderPage";
-
-const dropdownRight = ["contact infos", " select messages", "close discussions","mute notifications","ephemeral messages","remove the discussion","report","to block" ]
-const dropdownLeft = [" new group","new community", "important messages","select dicussion","parameters","disconnect"]
-
+import {
+  dropdownRight,
+  dropdownLeft,
+  dropdownBottomLeft,
+} from "../utils/data/mainpageDropdownLists";
+import DropDownR from "./mainLayoutPage/DropdownR";
 
 const Discossions = () => {
-  
+  const [showDropdrownleft, setShowDropdownleft] = useState<boolean>(false);
   const [showDropdrownright, setShowDropdownright] = useState<boolean>(false);
+  // const [showDropdrownleft, setShowDropdownleft] = useState<boolean>(false);
+  const [showDropdrownBottonL, setShowDropdrownBottonL] =
+    useState<boolean>(false);
 
+  const dropdownRef = useRef<HTMLUListElement>(null);
+  const ref = useRef<HTMLDivElement>(null);
+
+  const handleClickOutSide = (event: any) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+      setShowDropdownleft(false);
+      setShowDropdownright(false);
+      setShowDropdrownBottonL(false);
+    }
+  };
+
+  useEffect(() => {
+    if (ref.current !== null)
+      ref.current.addEventListener("click", handleClickOutSide);
+    return () => document.removeEventListener("click", handleClickOutSide);
+  }, []);
 
   return (
     <div className="flex w-full">
       <div className="bg-white w-[30vw] h-screen">
         <div>
-          {/* <div className="flex items-center max-h-16 justify-between bg-bgGray w-full h-max-5 px-3 py-2 border-r">
+          <div className="flex items-center max-h-16 justify-between bg-bgGray w-full h-max-5 px-3 py-2 border-r">
             <Avatar
               onClick={() => alert("clicked")}
               profilePicture="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=600"
@@ -34,17 +55,25 @@ const Discossions = () => {
               <button className="text-2xl text-gray-600">
                 <MdGroups2 />
               </button>
-              <button className="text-2xl text-gray-600 relative rounded-full" onClick={()=>setShowDropdownleft(prev => !prev)}>
+              <button
+                className="text-2xl text-gray-600 relative rounded-full"
+                onClick={() => setShowDropdownleft((prev) => !prev)}
+              >
                 <HiDotsVertical />
               </button>
-              {showDropdrownleft &&  <DropDown dropdownList ={dropdownLeft} />}
+
+              {showDropdrownleft && (
+                <DropDown dropdownList={dropdownLeft} ref={dropdownRef} />
+              )}
             </div>
-          </div> */}
-          <HeaderMainPageL/>
+          </div>
           <div></div>
         </div>
       </div>
-      <div className="w-[70vw] bg-[url('https://w0.peakpx.com/wallpaper/557/521/HD-wallpaper-whatsapp-v-background-doodle-pattern-patterns-whatsapp-thumbnail.jpg')]">
+      <div
+        ref={ref}
+        className="w-[70vw] bg-[url('https://w0.peakpx.com/wallpaper/557/521/HD-wallpaper-whatsapp-v-background-doodle-pattern-patterns-whatsapp-thumbnail.jpg')]"
+      >
         <div className="flex items-center bg-bgGray max-h-16 justify-between w-full h-max-5 px-3 py-2 ">
           <div className="flex gap-3">
             <Avatar
@@ -62,30 +91,53 @@ const Discossions = () => {
             <button className="text-2xl text-gray-600">
               <GoSearch />
             </button>
-            <button className="text-2xl text-gray-600" onClick={()=> setShowDropdownright(prev => !prev)}>
+            <button
+              className="text-2xl text-gray-600"
+              onClick={() => setShowDropdownright((prev) => !prev)}
+            >
               <HiDotsVertical />
             </button>
-            {/* {showDropdrownright && <DropDown dropdownList ={dropdownRight} />} */}
+            {showDropdrownright && (
+              <DropDown dropdownList={dropdownRight} ref={dropdownRef} />
+            )}
           </div>
         </div>
 
-        <form
-          action=""
-          className="flex items-center bg-bgGray h-[] fixed bottom-0 w-full py-2 px-4 gap-5"
-        >
-          <IoMdAdd className="text-2xl text-gray-700" />
+        <div className="flex items-center bg-bgGray h-[] fixed bottom-0 w-full py-2 px-4 gap-5">
+          {showDropdrownBottonL && <DropDownR ref={dropdownRef} />}
+          <BsEmojiSmile className="text-2xl text-gray-700" />
+          <button
+            type="button"
+            onClick={() => {
+              setShowDropdrownBottonL((prev) => !prev);
+              console.log("clicked third");
+            }}
+            className={
+              showDropdrownBottonL
+                ? " bg-zinc-400 rounded-full transition-opacity-1 duration-150 ease-in"
+                : "w-fit -rotate-45"
+            }
+          >
+            <IoMdAdd
+              className={
+                showDropdrownBottonL
+                  ? "text-2xl m-2 text-gray-900 rotate-45 transition-opacity-1 duration-150 ease-in"
+                  : "text-2xl m-2 text-gray-700 -rotate-45"
+              }
+            />
+          </button>
+
           <div className="flex bg-white items-center rounded-md gap-5 p-1">
-            <BsEmojiSmile className="text-2xl text-gray-700" />
             <input
               type="text"
-              className="w-[57vw] my-2 outline-none text-gray-600"
+              className="w-[57vw] my-2 outline-none text-gray-600 px-3 "
               placeholder="Type a message"
             />
           </div>
           <button className="text-2xl ">
             <IoSendSharp />
           </button>
-        </form>
+        </div>
       </div>
     </div>
   );
--- components/mainLayoutPage/DropDown.tsx
+++ components/mainLayoutPage/DropDown.tsx
@@ -1,24 +1,27 @@
-import * as React from 'react';
+import React, { forwardRef } from "react";
 
 export interface IAppProps {
-    dropdownList: string[],
-    // ref: React.MutableRefObject<HTMLUListElement | null>
+  dropdownList: string[];
 }
 
- const DropDown =(props:IAppProps) => {
-
-    console.log("in the drop d");
+const DropDown = forwardRef<HTMLUListElement, IAppProps>((props, ref) => {
+  console.log("in the drop d");
 
   return (
-  <ul className="absolute -translate-x-48 mt-8 py-2 w-[250px] bg-white rounded-md shadow-xl z-20">
-    {props.dropdownList.map((value, index) => (
-        <li key={index} className="w-64  text-sm capitalize text-gray-700 hover:bg-bgGray hover:text-black py-[10px] px-[24px] hover:w-full cursor-pointer  nowrap">
-      {value}
-    </li>
-    ))}
-  </ul>
+    <ul
+      ref={ref}
+      className="absolute mt-8 py-2 w-[250px] bg-white rounded-md shadow-xl  delay-7000 transform transition-duration-10000   -translate-x-48 -scale-30  ease-in-out"
+    >
+      {props.dropdownList.map((value, index) => (
+        <li
+          key={index}
+          className="w-64  text-sm capitalize text-gray-700 hover:bg-bgGray hover:text-black py-[10px] px-[24px] hover:w-full cursor-pointer  nowrap"
+        >
+          {value}
+        </li>
+      ))}
+    </ul>
   );
-};
+});
 
 export default DropDown;
-// absolute top-20 left-72 
