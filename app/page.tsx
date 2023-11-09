8352e4754da2757515d47428b3975a478bab07c1
frckbrice <maebrie2017@gmail.com>
2023-11-09 07:12:30 +0100

    adding local modifications

--- app/page.tsx
+++ app/page.tsx
@@ -22,6 +22,7 @@ export default async function Index() {
   };
 
   const isSupabaseConnected = canInitSupabaseClient();
+  if (isSupabaseConnected) console.log("supabase is connected successfully");
 
   return (
     <div className="flex-1 h-[80vh] w-[100%] flex flex-col items-center mx-auto rounded-sm">
