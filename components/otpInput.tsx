5f089f3409903b906cdca7370ff939e36bbd4011
nsamedaisy <nsamedaisy@gmail.com>
2023-11-15 11:22:41 +0100

    input field adjustment

--- components/otpInput.tsx
+++ components/otpInput.tsx
@@ -1,16 +1,33 @@
-import React, { FC, useState } from "react";
+import React, { FC, useState, useRef } from "react";
 
 interface Props {}
 
 const OTPField: FC<Props> = (props): JSX.Element => {
   const [otp, setOtp] = useState<string[]>(new Array(6).fill(""));
+  const inputRefs = useRef<(HTMLInputElement | null)[]>([]);
+
+  const handleInputChange = (index: number, value: string) => {
+    const newOtp = [...otp];
+    newOtp[index] = value;
+
+    setOtp(newOtp);
+
+    // Move to the next input field
+    if (value !== "" && index < otp.length - 1) {
+      inputRefs.current[index + 1]?.focus();
+    }
+  };
+
   return (
     <div className="flex justify-center items-center space-x-2">
       {otp.map((_, index) => {
         return (
           <React.Fragment key={index}>
             <input
-              type="number"
+              ref={(ref) => (inputRefs.current[index] = ref)}
+              value={otp[index]}
+              onChange={(e) => handleInputChange(index, e.target.value)}
+              maxLength={1}
               className="w-12 h-12 border-2 rounded-md bg-transparent outline-none text-center font-semibold text-xl border-gray-400 focus:border-gray-700 focus:text-gray-700 text-gray-400 transition"
             />
             {index === otp.length - 1 ? null : (
