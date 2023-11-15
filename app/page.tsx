68b1d7917be7b86f5e433428e368f5a6f52f2f07
gmarvis <sgmarvis@gmail.com>
2023-11-15 15:59:58 +0100

    new change on th e app page

--- app/page.tsx
+++ app/page.tsx
@@ -30,9 +30,9 @@ export default async function Index() {
 
   return (
     <div className="flex-1 h-[80vh] w-[100%] flex flex-col items-center mx-auto rounded-sm ">
-      <Signupb />
+      {/* <Signupb /> */}
       {/* <Signup /> */}
-      {/* <Discossions /> */}
+      <Discossions />
     </div>
   );
 }
