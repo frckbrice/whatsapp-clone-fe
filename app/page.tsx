103dff2e99a96517726599333ddaf298a1df4ad8
nsamedaisy <nsamedaisy@gmail.com>
2023-11-17 15:11:54 +0100

    responsive fix for discussion page

--- app/page.tsx
+++ app/page.tsx
@@ -30,7 +30,7 @@ export default async function Index() {
 
   return (
     <div className="flex-1 h-[80vh] w-[100%] flex flex-col items-center mx-auto rounded-sm ">
-      {/* <Signupb /> */}
+      <Signupb />
       {/* <Signup /> */}
       {/* <Discossions /> */}
     </div>
