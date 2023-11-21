edad1b0eb9b41f4ac07872fac2badda07c524249
Gael Tikeng <Gealinho@yahoo.fr>
2023-11-21 15:48:11 +0100

    fixed some bugs

--- utils/queries/fetchGroupsOfSingleUser.tsx
+++ utils/queries/fetchGroupsOfSingleUser.tsx
@@ -2,13 +2,13 @@ import { supabase } from "../supabase/client"
 
 // get all the groups to which a signup user belongs to
 const fetchGroupsOfSingleUser = async () => {
-  // const sender = JSON.parse(localStorage.getItem("sender") || '{}')
-  const sender: string = '277db867-1474-45ac-908f-e3e9e6c4a5f6'
+  const sender = JSON.parse(localStorage.getItem("sender") || '{}')
+  // const sender: string = '277db867-1474-45ac-908f-e3e9e6c4a5f6'
   const { data, error } = await supabase
     .from('roomuser')
     .select('room_id')
-    // .eq("user_id", sender.id)
-    .eq('user_id', sender)
+    .eq("user_id", sender.id)
+    // .eq('user_id', sender)
   
     if (error) console.log("could not search for all groups to which user belongs to", error)
     if (data) {
