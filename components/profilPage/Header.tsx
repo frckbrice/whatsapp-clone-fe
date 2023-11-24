c2b1b8d244002be7a091f6de0247fe9ea0e69b6a
Gael Tikeng <Gealinho@yahoo.fr>
2023-11-24 15:09:48 +0100

    removing material ui

--- components/profilPage/Header.tsx
+++ components/profilPage/Header.tsx
@@ -1,22 +1,22 @@
-"use client ";
+// "use client ";
 
-import * as React from "react";
-import { Switch, FormControlLabel } from "@mui/material";
+// import * as React from "react";
+// import { Switch, FormControlLabel } from "@mui/material";
 
-type Props = {
-  switchTheme: any;
-  label: string;
-};
+// type Props = {
+//   switchTheme: any;
+//   label: string;
+// };
 
-export default function Header({ switchTheme, label }: Props) {
-  return (
-    <div className=" w-full flex items-center justify-end">
-      <FormControlLabel
-        control={
-          <Switch onChange={switchTheme} name="gilard" color="primary" />
-        }
-        label={`${label}`}
-      />
-    </div>
-  );
-}
+// export default function Header({ switchTheme, label }: Props) {
+//   return (
+//     <div className=" w-full flex items-center justify-end">
+//       <FormControlLabel
+//         control={
+//           <Switch onChange={switchTheme} name="gilard" color="primary" />
+//         }
+//         label={`${label}`}
+//       />
+//     </div>
+//   );
+// }
