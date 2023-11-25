3ae5071687b2ffda39e2b86f77f7aaa38ab90be5
nsamedaisy <nsamedaisy@gmail.com>
2023-11-25 11:34:34 +0100

    signup/welcome page updates

--- app/[signup]/page.tsx
+++ app/[signup]/page.tsx
@@ -1,23 +1,11 @@
 "use client";
-import Image from "next/image";
 import React, { useState } from "react";
-import { CiMenuKebab } from "react-icons/ci";
-import { GrSettingsOption } from "react-icons/gr";
-import { useParams } from "next/navigation";
 import { useRouter } from "next/navigation";
 import { supabase } from "@/utils/supabase/client";
-import Pulsation from "./component/PulseLoader";
 
 const Signupb = () => {
-  const [error, setError] = useState("");
-  const [success, setSuccess] = useState("");
   const router = useRouter();
   const [isloading, setIsloading] = useState(false);
-
-  // const generateToken = async (id: any) => {
-  //   const token = await supabase.auth.setSession({id})
-  //   return token
-  // }
 
   const handleInputChange = async () => {
     setIsloading(true);
@@ -25,44 +13,19 @@ const Signupb = () => {
 
     localStorage.setItem("email", JSON.stringify(googleUser.user.email))
 
-    const { data, error } = await supabase
-      .from('user')
-      .insert({ email: googleUser.user.email, name: googleUser.user.user_metadata.name, image: googleUser.user.user_metadata.picture, phone: googleUser.user.identities.phone  })
+    const { data, error } = await supabase.from("user").insert({
+      email: googleUser.user.email,
+      name: googleUser.user.user_metadata.name,
+      image: googleUser.user.user_metadata.picture,
+      phone: googleUser.user.identities.phone,
+    });
 
     if (error) console.log('an error occured while sending user', error)
     
       console.log('data from DB', data)
     
     router.push('/discussions')
-
-    // const email = localStorage.getItem("email");
-    // const random = one + two + three + four + five + six;
-    // let code = JSON.stringify(params.signup);
-    // let sentCode = code.slice(10, 16);
-
-    // if (random == sentCode) {
-    //   const { data, error } = await supabase
-    //     .from("user")
-    //     .insert({ email: email });
-
-    //   console.log(data);
-
-    //   if (error) {
-    //     console.log(error);
-    //     setError("Email address already exist");
-    //     setIsloading(false);
-    //     return;
-    //   }
-    //   //  if (data) console.log(data);
-    //   router.push("/discussions");
-    // } else {
-    //   setError("Invalid code");
-    //   console.log("invalid");
-    //   setIsloading(false);
-    //   return;
-    // }
-
-
+    setIsloading(false)
   };
 
   return (
